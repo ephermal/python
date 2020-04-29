d5c28e28bc9e30167de0fb9b4d8817a558cb9a20
Gerald Reid <gerald.reid@gmail.com>
2020-04-29 11:39:33 -0500

    Update rotateKeys3.py

--- rotateKeys3.py
+++ rotateKeys3.py
@@ -1,9 +1,8 @@
 #Script to rotate keys for aws
-#Some arguments are required others are option
-#Requires python3
+#Some arguments are required others are optional
+#Jerry Reid
 #Version 1 : Create, Delete, Rotate Keys - Working
 #Version 2 : Edit aws credentials file (work in progress)
-
 
 import argparse
 import boto3
@@ -11,16 +10,13 @@ import fileinput
 import sys
 from botocore.exceptions import ClientError, ParamValidationError
 from boto3 import Session
-
-
 iam_client = boto3.client('iam')
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--username", help="An IAM username, e.g. key_rotator.py --username <username>", required=True)
     parser.add_argument("-k", "--key", help="An AWS access key, e.g. key_rotator.py --key <access_key>")
-    parser.add_argument("--delete", help="Deletes an access key", action="store_true")
-    parser.add_argument("-s", "--show", help="Shows Current Keys", action="store_true")
+    parser.add_argument("-d", "--delete", help="Deletes an access key", action="store_true")
     parser.add_argument("-r", "--rotate", help="rotates current key",action="store_true")
     parser.add_argument("-p", "--profile", help="Choose the profile from aws config file ", nargs='?', const="default", type=str, required=True)
     args = parser.parse_args()
@@ -29,16 +25,8 @@ def main():
     aws_profile = boto3.session.Session(profile_name=profile)
     aws_access_key = get_key(username,aws_profile)
     try:
-        keys = iam_client.list_access_keys(UserName=username)
-        inactive_keys = 0
-        active_keys = 0
-        for key in keys['AccessKeyMetadata']:
-            if key['Status']=='Inactive': inactive_keys = inactive_keys + 1
-            elif key['Status']=='Active': active_keys = active_keys + 1
         if args.delete:
             delete_key(aws_access_key, username,aws_profile)
-        elif args.show:
-            get_key(username,profile)
         elif args.rotate:
             rotate_key(username,aws_profile)
         else:
@@ -46,8 +34,8 @@ def main():
     except ClientError as e:
         print(("The user with the name %s cannot be found." % username))
     except ParamValidationError as e:
-        print ("you need to use the proper arguments i.e python3 rotateKeys3.py --username username@salesforce.com -p profile ")
-    return (username, profile,aws_access_key)
+        print ("you need to use the proper arguments i.e python3 rotateKeys3.py -u username@salesforce.com -p profile ")
+    return (username,profile,aws_access_key)
 
 def get_key(username,aws_profile):
         session = aws_profile
@@ -84,7 +72,7 @@ def delete_key(access_key,username,aws_profile):
             if i == 'y':
                 iam_client.delete_access_key(UserName=username, AccessKeyId=access_key)
                 print((access_key + " has been deleted."))
-                return old_access_key
+                return access_key
             elif i == 'n':
                 exit()
     except ClientError as e:
@@ -96,6 +84,9 @@ def rotate_key(username,aws_profile):
     delete_key(aws_access_key,username,aws_profile)
     print ("creating new key")
     create_key(username,aws_access_key,aws_profile)
+
+#Edit AWS Credentials file
+
 
 if __name__ == "__main__":
     main()
