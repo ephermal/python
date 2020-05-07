1b72ced2a5450c390155659947e3c1017b936b86
Gerald Reid <gerald.reid@gmail.com>
2020-05-07 13:41:52 -0500

    Update getSecrets3.py

--- getSecrets3.py
+++ getSecrets3.py
@@ -4,7 +4,6 @@
 
 import argparse
 import boto3
-import base64
 from botocore.exceptions import ClientError, ParamValidationError
 iam_client = boto3.client('iam')
 
@@ -19,7 +18,7 @@ def main():
                         nargs='?', const="us-west-2", type=str, required=True)
     parser.add_argument("-s", "--secret",
                         help="Please specify secret name ",
-                        nargs='?', type=str, required=True)
+                        nargs='?', required=True)
     args = parser.parse_args()
     region = args.region
     profile = args.profile
@@ -48,33 +47,20 @@ def get_secret(secret, region, profile):
             # Secrets Manager can't decrypt the protected secret text
             # using the provided KMS key.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+            print("secrets manager cannot decrypt using provided KMS")
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
             # An error occurred on the server side.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current
-            # state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+            print("Error on Server side, try again later")
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+            print("Cannot find secret; please check secret name and region")
     else:
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one
         # of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-        else:
-            decoded_binary_secret = base64.b64decode(
-                get_secret_value_response['SecretBinary'])
+        secret = get_secret_value_response['SecretString']
         print(secret)
 
 
